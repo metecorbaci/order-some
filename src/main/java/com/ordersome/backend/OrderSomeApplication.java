34fe04d712ebbd827fe68c7b8aad10df6b44123a
Mete Çorbacı <metecorbaci22@gmail.com>
2024-05-28 17:31:08 +0300

    First commit

---
+++ src/main/java/com/ordersome/backend/OrderSomeApplication.java
@@ -0,0 +1,13 @@
+package com.ordersome.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordersome/backend/OrderSomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordersome.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
